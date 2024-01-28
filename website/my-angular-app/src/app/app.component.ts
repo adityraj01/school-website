0fa09bd6286ccc35993c900b0fed68514b47856d
Adity Shaw <adityshaw99@gmail.com>
2024-01-28 23:16:52 +0530

    commit1

---
+++ website/my-angular-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,
+            MatCardModule,
+            MatButtonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'my-angular-app';
+}
